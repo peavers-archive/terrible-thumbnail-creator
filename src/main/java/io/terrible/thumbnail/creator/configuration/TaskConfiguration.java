ffbe3d60df20b221864d162bf11fcf143721a114
Chris Turner <chris@forloop.space>
2020-01-19 11:01:45 +0000

    fix: bad hook install

--- src/main/java/io/terrible/thumbnail/creator/configuration/TaskConfiguration.java
+++ src/main/java/io/terrible/thumbnail/creator/configuration/TaskConfiguration.java
@@ -29,6 +29,14 @@ public class TaskConfiguration {
       JCommander.newBuilder().addObject(this.args).build().parse(args);
 
       thumbnailService.createThumbnails(Paths.get(this.args.getVideo()), this.args.getCount());
+
+
+
+
+
     };
+
+
+
   }
 }
