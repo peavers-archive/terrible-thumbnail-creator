abc2b568728e99bf0aef65196d978e4add34883b
Chris Turner <chris@forloop.space>
2020-04-03 15:39:51 +0100

    fix: Add web container for reporting

--- src/main/java/io/terrible/thumbnail/creator/configuration/TaskConfig.java
+++ src/main/java/io/terrible/thumbnail/creator/configuration/TaskConfig.java
@@ -1,28 +1,20 @@
 /* Licensed under Apache-2.0 */
 package io.terrible.thumbnail.creator.configuration;
 
-import com.beust.jcommander.JCommander;
-import io.terrible.thumbnail.creator.Application;
-import io.terrible.thumbnail.creator.domain.Result;
 import io.terrible.thumbnail.creator.service.MessageService;
 import io.terrible.thumbnail.creator.service.ThumbnailService;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.support.GenericMessage;
 
 /** @author Chris Turner (chris@forloop.space) */
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class TaskConfig {
-
-  private final Application.Args args = new Application.Args();
 
   private final ThumbnailService thumbnailService;
 
@@ -38,21 +30,6 @@ public class TaskConfig {
 
   @Bean
   public CommandLineRunner commandLineRunner() {
-
-    return args -> {
-      JCommander.newBuilder().addObject(this.args).build().parse(args);
-
-      final String path = this.args.getVideo();
-      final int count = this.args.getCount();
-
-      final ArrayList<String> thumbnails =
-          thumbnailService.createThumbnails(Paths.get(path), count);
-
-      final Result result = Result.builder().videoPath(path).thumbnails(thumbnails).build();
-
-      messageService.send(new GenericMessage<>(result));
-
-      System.exit(0);
-    };
+    return new TaskRunner(thumbnailService, messageService);
   }
 }
---
+++ src/main/java/io/terrible/thumbnail/creator/configuration/TaskRunner.java
@@ -0,0 +1,41 @@
+/* Licensed under Apache-2.0 */
+package io.terrible.thumbnail.creator.configuration;
+
+import com.beust.jcommander.JCommander;
+import io.terrible.thumbnail.creator.Application;
+import io.terrible.thumbnail.creator.domain.Result;
+import io.terrible.thumbnail.creator.service.MessageService;
+import io.terrible.thumbnail.creator.service.ThumbnailService;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.stereotype.Component;
+
+/** @author Chris Turner (chris@forloop.space) */
+@Component
+@RequiredArgsConstructor
+public class TaskRunner implements CommandLineRunner {
+
+  private final Application.Args args = new Application.Args();
+
+  private final ThumbnailService thumbnailService;
+
+  private final MessageService messageService;
+
+  @Override
+  public void run(final String... strings) {
+
+    JCommander.newBuilder().addObject(this.args).build().parse(strings);
+
+    final String path = this.args.getVideo();
+    final int count = this.args.getCount();
+
+    final ArrayList<String> thumbnails = thumbnailService.createThumbnails(Paths.get(path), count);
+
+    final Result result = Result.builder().videoPath(path).thumbnails(thumbnails).build();
+
+    messageService.send(new GenericMessage<>(result));
+  }
+}
